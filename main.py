3180bd8d88a7b220030770154200c92f3d0c9036
Equinox2654 <thibishaan@gmail.com>
2025-05-17 17:29:20 +1000

    Added function to get the number of characters in a file

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from stats import get_num_words
+import stats
 
 # book bot main
 
@@ -9,6 +9,8 @@ def get_book_text(path_to_file):
 
 def main():
     print(get_book_text("books/frankenstein.txt"))
-    print(f"{get_num_words("books/frankenstein.txt")} words found in the document.")
+    print(f"{stats.get_num_words("books/frankenstein.txt")} words found in the document.")
+    print("")
+    print(f"The different characters found are {stats.get_num_characters("books/frankenstein.txt")}.")
 
 main()
--- stats.py
+++ stats.py
@@ -1,6 +1,22 @@
+# Finds stats for files
+
 def get_num_words(path_to_file):
     """Find the amount of words in a text file"""
     with open(path_to_file) as f:
         words = (f.read()).split()
         num_words = len(words)
     return num_words
+
+# counts the amount of times a number appears in a file
+
+def get_num_characters(path_to_file):
+    """Finds the amount of all characters in a text file"""
+    characters = {}
+    with open(path_to_file) as f:
+        file = (f.read()).lower()
+        for char in file:
+            if char in characters:
+                characters[char] += 1
+            else:
+                characters[char] = 1
+    return characters
